40a0631a9ad137c3c261f8ed558ccbe12a1bc1bb
parksuha <parksuha@msu.edu>
2024-10-08 20:01:53 -0400

    db mgmt update

--- utils/db_management.py
+++ utils/db_management.py
@@ -2,15 +2,15 @@ from gcs import GCS
 from scraper import get_video_links
 from tqdm import tqdm
 
-
 def gather_audio_data(db: GCS) -> str:
     """
     Complete function to download YouTube videos, convert files (MP4 -> WAV, WAV -> MIDI),
     and upload them to corresponding GCS buckets.
 
-    :param GCS db: database object (GCS)
+    :param GCS db: database object (GCS), defaulted to gcs
     :return str: work complete message
     """
+    
     db.blob_count_refresh()
     
     video_links = get_video_links()
@@ -20,3 +20,7 @@ def gather_audio_data(db: GCS) -> str:
     
     db.blob_count_refresh()
     return f"Uploading Audio Files Complete.\nTotal WAV Files: {db.wav_blob_count}\nTotal MIDI Files: {db.midi_blob_count}"
+
+if __name__ == '__main__':
+    gcs = GCS()
+    print(gather_audio_data(db=gcs))
